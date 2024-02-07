72ab23c75d71c73bd25063f9644d6e5cdc5832d9
NataliaJG123 <158162830+NataliaJG123@users.noreply.github.com>
2024-02-07 12:46:08 +0100

    Create practica1.R

---
+++ practica1.R
@@ -0,0 +1,34 @@
+#Ej 1
+edades_descubrimientos <-c(1900,2003,1978,1901,2020,1842)
+mean(edades_descubrimientos)
+
+#Ej 2
+cantidad_artefactos <-c(1,2,3,4,5,6,7,8,9)
+sum(cantidad_artefactos)
+
+#Ej 3
+profundidad_hallazgos <-c(1,2,3,4,5,6,7,8,9,10)
+max(profundidad_hallazgos)
+
+#Ej 4
+materiales_encontrados <-c("Ceramica", "Restos de fauna", "Escoria", "Vidrio")
+length(materiales_encontrados)
+
+#Ej 5
+años_excavaciones <-c(1900,1800,1560,2021,2003)
+lenght(años_excavaciones)
+
+#Ej 6
+matriz1<-matrix(c(1,2,3,4,5,6),
+nrow = 2, 
+byrow = TRUE)
+
+colnames(matriz1)<-c(sitios_arqueologicos)
+rownames(matriz1)<-c(años)
+
+años<-c(2002,2001,2003,2004)
+sitios_arqueologicos<-c("La Pileta", "El Gandul","Alcazar de Sevilla","Atapuerca","Mas dels Frares")
+
+
+
+  
